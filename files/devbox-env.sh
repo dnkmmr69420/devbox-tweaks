c0c74fe65c02e431121096e076a79fffdf0c40cd
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-06-11 14:46:20 -0500

    Create devbox-env.sh

---
+++ files/devbox-env.sh
@@ -0,0 +1,2 @@
+export PATH="$HOME/.local/share/devbox/global/current/.devbox/nix/profile/default/bin:$PATH"
+export XDG_DATA_DIRS="$HOME/.local/share/devbox/global/current/.devbox/nix/profile/default/share:$XDG_DATA_DIRS"
